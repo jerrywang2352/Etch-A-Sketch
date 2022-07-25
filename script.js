4d07bc3cd7004586860632807693a466fbbefb65
Jerry Wang <jerry.wang2352@gmail.com>
2022-07-25 03:44:38 -0400

    Add hover event

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const container = document.querySelector("div.container"); 
-container.textContent = 0; 
 for(let i = 0; i < 16; i++) {
     for(let k = 0; k < 16; k++) {
         let squareDiv = document.createElement("div"); 
-        squareDiv.setAttribute("row",i); 
-        squareDiv.setAttribute("col",k); 
+        squareDiv.setAttribute("row",i+1); 
+        squareDiv.setAttribute("col",k+1); 
+        squareDiv.classList.add("square-div"); 
         container.appendChild(squareDiv); 
     }
 }
 
+const squareDivs = document.querySelectorAll(".square-div");
+squareDivs.forEach((squareDiv) => {
+   squareDiv.addEventListener("mouseover",(e)=>
+   e.target.classList.add("hover"));  
+});
 
