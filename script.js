e4ee198d94b7d2a558dc1640a9261367b284f654
Jerry Wang <jerry.wang2352@gmail.com>
2022-07-27 02:48:34 -0400

    Add a slider

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const container = document.querySelector("div.container"); 
+const slider = document.querySelector("input[type=range]");
+const sliderOutput = document.querySelector(".grid-size"); 
+
+//display slider value 
+sliderOutput.textContent = slider.value + " x " + slider.value; 
+slider.oninput = () => {
+    sliderOutput.textContent = slider.value + " x " + slider.value; 
+}
+
+
+//check if mouse is down or not
 let isDown = false;
 document.querySelector(".container").onmousedown = () => isDown = true; 
 document.querySelector(".container").onmouseup = () => isDown = false;
 
+//create the grid 
 for(let i = 0; i < 16; i++) {
     for(let k = 0; k < 16; k++) {
         let squareDiv = document.createElement("div"); 
@@ -13,6 +25,7 @@ for(let i = 0; i < 16; i++) {
     }
 }
 
+//etch if mouse down and over
 const squareDivs = document.querySelectorAll(".square-div");
 squareDivs.forEach((squareDiv) => { 
    squareDiv.addEventListener("mouseenter",(e) => {
